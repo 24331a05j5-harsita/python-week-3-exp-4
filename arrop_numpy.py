63ab7df26b0a28a2b24c2a7c5091781a7c4c5104
24331a05j5-harsita <24331a05j5@mvgrce.edu.in>
2025-12-25 21:43:55 +0530

    Add files via upload

---
+++ arrop_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+lst1=[1,2,3]
+lst2=[2,4,6]
+print("sum of list 1 and list 2(concatenation)is:",lst1+lst2)
+nplst1=np.array(lst1)
+nplst2=np.array(lst2)
+print("sum:",nplst1+nplst2)
+print("multiplication:",nplst1*nplst2)
+print("diff:",nplst1-nplst2)
+print("div:",nplst1/nplst2)
